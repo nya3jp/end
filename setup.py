ca0b697459f74c7a654f4444d016d2dec3f1be7f
Shuhei Takahashi <takahashi.shuhei@gmail.com>
2016-10-30 16:48:53 +0900

    Change copyright line.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Shuhei Takahashi All rights reserved
+# Copyright 2016 Shuhei Takahashi
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
